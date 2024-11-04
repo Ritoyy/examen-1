facd88beb35c77111043dfff9bc80a4241b0e129
Ritoyy <yassergf@outlook.com>
2024-11-04 16:55:16 -0600

    Ingredientes y uso

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-<<<<<<< HEAD
-import Product from '../views/Product.vue'
 import ProductDetail from '@/components/ProductDetails.vue' // Path to your product detail page
 import ProductList from '../views/ProductList.vue'
-=======
 import Product from '../views/ProductList.vue'
 import Checkout from '../views/CheckoutPage.vue'
->>>>>>> a2cdf1701fb6d4730f3c8c4447dd2ee2b2891a10
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,12 +14,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/product',
-      name: 'product',
-      component: Product,
-    },
-    {
-<<<<<<< HEAD
       path: '/productlist',
       name: 'productlist',
       component: ProductList,
@@ -33,12 +23,8 @@ const router = createRouter({
       name: 'ProductDetail',
       component: ProductDetail,
       props: true, // This allows passing the route parameter as a prop to the component
-=======
-      path: '/checkout',
-      name: 'checkout',
-      component: Checkout,
->>>>>>> a2cdf1701fb6d4730f3c8c4447dd2ee2b2891a10
     },
+    { path: '/checkout', name: 'checkout', component: Checkout },
   ],
 })
 
