3d237eab8ea9baf46eccd7dcc03d9c15e5d311ce
Jordix69 <laracisnerosjairo@gmail.com>
2024-11-04 17:35:23 -0600

    ALSO LIKE

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductDetail from '@/components/ProductDetails.vue' // Path to your product detail page
 import ProductList from '../views/ProductList.vue'
-import Product from '../views/ProductList.vue'
 import Checkout from '../views/CheckoutPage.vue'
 import Cart from '../views/CartView.vue'
+import Also from '../components/AlsoLike.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,15 +25,13 @@ const router = createRouter({
       component: ProductDetail,
       props: true, // This allows passing the route parameter as a prop to the component
     },
-<<<<<<< HEAD
     {
       path: '/cart',
       name: 'cart',
       component: Cart,
     },
-=======
     { path: '/checkout', name: 'checkout', component: Checkout },
->>>>>>> facd88beb35c77111043dfff9bc80a4241b0e129
+    { path: '/also', name: 'also', component: Also },
   ],
 })
 
