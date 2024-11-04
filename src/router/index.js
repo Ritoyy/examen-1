a2cdf1701fb6d4730f3c8c4447dd2ee2b2891a10
Jordix69 <laracisnerosjairo@gmail.com>
2024-11-03 22:33:28 -0600

    Checkout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/ProductList.vue'
+import Checkout from '../views/CheckoutPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/product',
       name: 'product',
       component: Product,
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: Checkout,
     },
   ],
 })
