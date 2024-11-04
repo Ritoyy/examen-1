9b33e6a6609027defd90ed42336eaf932432c4d8
Jordix69 <laracisnerosjairo@gmail.com>
2024-11-03 20:08:11 -0600

    ProductList

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Product from '../views/Product.vue'
+import Product from '../views/ProductList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,12 @@
         <a href="#" style="color: gray; margin-inline: 30px">ABOUT US</a>
         <a href="#" style="color: gray">GLASS SKIN DUO</a>
       </div>
-      <div class="col"><img src="https://i0.wp.com/beautyfeed.org/wp-content/uploads/2024/08/0-02-05-49d2ac187d2d7f767ae6122638d9714c0d32759cc6b44e9bcd74fe742e3619ab_f9b359a9a4080609.png?fit=1553%2C685&ssl=1" alt="logo" /></div>
+      <div class="col">
+        <img
+          src="https://i0.wp.com/beautyfeed.org/wp-content/uploads/2024/08/0-02-05-49d2ac187d2d7f767ae6122638d9714c0d32759cc6b44e9bcd74fe742e3619ab_f9b359a9a4080609.png?fit=1553%2C685&ssl=1"
+          alt="logo"
+        />
+      </div>
       <div class="col" style="text-align: end">
         <a href="#" style="color: gray; margin-inline: 30px">CART</a>
         <a href="#" style="color: gray">SEARCH</a>
